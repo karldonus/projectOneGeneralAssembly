c7790ed10b350da064cbeafd73571c2eff94f972
Karl Sakas <donusdonut@Karls-MacBook-Pro.local>
2015-07-14 11:34:52 -0400

    resolved ace issue for player. next: resolve ace for dealer. solved thru push into dummy array and inarray if statement.

--- script.js
+++ script.js
@@ -5,6 +5,7 @@
 
 var deckNum = [["Two of Clubs",2],["Three of Clubs",3],["Four of Clubs",4],["Five of Clubs",5],["Six of Clubs",6],["Seven of Clubs",7],["Eight of Clubs",8],["Nine of Clubs",9],["Ten of Clubs",10],["Jack of Clubs",10],["Queen of Clubs",10],["King of Clubs",10],["Ace of Clubs",11],["Two of Hearts",2],["Three of Hearts",3],["Four of Hearts",4],["Five of Hearts",5],["Six of Hearts",6],["Seven of Hearts",7],["Eight of Hearts",8],["Nine of Hearts",9],["Ten of Hearts",10],["Jack of Hearts",10],["Queen of Hearts",10],["King of Hearts",10],["Ace of Hearts",11],["Two of Spades",2],["Three of Spades",3],["Four of Spades",4],["Five of Spades",5],["Six of Spades",6],["Seven of Spades",7],["Eight of Spades",8],["Nine of Spades",9],["Ten of Spades",10],["Jack of Spades",10],["Queen of Spades",10],["King of Spades",10],["Ace of Spades",11],["Two of Diamonds",2],["Three of Diamonds",3],["Four of Diamonds",4],["Five of Diamonds",5],["Six of Diamonds",6],["Seven of Diamonds",7],["Eight of Diamonds",8],["Nine of Diamonds",9],["Ten of Diamonds",10],["Jack of Diamonds",10],["Queen of Diamonds",10],["King of Diamonds",10],["Ace of Diamonds",11]];
 
+
 //Fisher Yates Method
 function shuffle(array) {
   var m = array.length, t, i;
@@ -20,7 +21,6 @@ function shuffle(array) {
   return array;
 }
 
-
 //Global Object Storage
 var els = {
         chipDisplay    : 200,
@@ -29,6 +29,8 @@ var els = {
         dealerCardCount: 2,
         playerCardCount: 2,
         playerTotal    : 0,
+        playerAceCheck : [],
+        dealerAceCheck : [],
 };
 
 //Loan Shark Mod
@@ -71,9 +73,10 @@ $("#hit").on("click", function(e){
       $(".playerHand").children().eq(0).html(deckNum[3][0]);
       $(".playerHidden").prepend("<div></div>");
       $(".playerHidden").children().eq(0).html(deckNum[3][1]);
+      els.playerAceCheck.push(deckNum[3][0].slice(0,3));
 
           calcPlayerTotal();
-          // acePlayerCalc();
+          switchPlayerAce();
               $(".playerTitle").html("Player's Hand: " + els.playerTotal.toString());
           checkBust();
           els.playerCardCount ++;
@@ -162,9 +165,18 @@ var checkWinner = function () {
                     //reshuffle();
 }}};
 
+//Check Player Ace
+var switchPlayerAce = function () {
+    if (els.playerTotal > 21) {
+          if ($.inArray("Ace", els.checkPlayerAce) !== 0) {
+                els.playerTotal = els.playerTotal - 10;
+}}};
+
+
 //Check Bust
 var checkBust = function () {
     if (els.playerTotal > 21) {
+
       if (els.chipDisplay < 0) {
             alert("Oh No! You Bust. Loan Shark Tony Broke Your Legs. Here's the Gambling Addiction Hotline: 1-800-522-4700.");
             els.wagerDisplay = 0;
@@ -187,11 +199,13 @@ var deal = function () {
     $(".playerHand").children().eq(0).html(deckNum[0][0]);
     $(".playerHidden").prepend("<div></div>");
     $(".playerHidden").children().eq(0).html(deckNum[0][1]);
+    els.playerAceCheck.push(deckNum[0][0].slice(0,3));
 
     $(".playerHand").prepend("<div></div>");
     $(".playerHand").children().eq(0).html(deckNum[1][0]);
     $(".playerHidden").prepend("<div></div>");
     $(".playerHidden").children().eq(0).html(deckNum[1][1]);
+    els.playerAceCheck.push(deckNum[1][0].slice(0,3));
 
         calcPlayerTotal();
             $(".playerTitle").html("Player's Hand: " + els.playerTotal.toString());
@@ -200,6 +214,7 @@ var deal = function () {
     $(".dealerHand").children().eq(0).html(deckNum[2][0]);
     $(".dealerHidden").prepend("<div></div>");
     $(".dealerHidden").children().eq(0).html(deckNum[2][1]);
+    els.dealerAceCheck.push(deckNum[2][0].slice(0,3));
 
     $(".dealerHand").prepend("<div></div>");
     $(".dealerHand").children().eq(0).html("???");
